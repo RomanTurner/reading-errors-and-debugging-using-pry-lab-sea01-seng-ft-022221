9003438ad073e7a75080b0a23a1336ea3ba8f0a9
Roman Turner <rn.turner@live.com>
2021-01-19 00:02:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,11 +12,10 @@ def prompt_user
 end
 
 def selection(num)
-  num.gets.chomp!
-  binding.pry
-  if num = 1
+
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
